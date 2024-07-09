5aad76991a223aa42bf7674a08ccd8426e0638fc
Xavier00014 <131310111+Xavier00014@users.noreply.github.com>
2024-07-09 10:30:20 -0400

    premier test

---
+++ processor.test.js
@@ -0,0 +1,8 @@
+const processor = require('./processor.js');
+
+describe("transmission processor", function(){
+    test("takes a string returns an object", function(){
+        let result = processor("9701::<489584872710>");
+        expect(typeof result).toEqual("object");
+    });
+})
