aef042f6a44cdfb80fee0701fbef1225449b165e
Xavier00014 <131310111+Xavier00014@users.noreply.github.com>
2024-07-09 10:38:54 -0400

    test 3

--- processor.test.js
+++ processor.test.js
@@ -11,4 +11,9 @@ describe("transmission processor", function(){
         expect(() => { processor("9701<489584872710>"); }).toThrow(expectedError);
     });
     
+    test("returns id in object", function () {
+        let result = processor("9701::<489584872710>");
+        expect(result.id).not.toEqual(undefined);
+    });
+    
 })
